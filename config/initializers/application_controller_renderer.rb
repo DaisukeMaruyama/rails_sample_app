53e5573414f5df2782fc3083202f8f682791a2f8
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-01-25 01:30:52 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '959cb4ddfa952be305488ba539a70b1996d79cd48bb9069dc2447bcf05a7ed35d0c9e7795f319601ca021b7c6cb6dd25e24b5ff2a1c8ee9ec0c5c7f31870786b'
